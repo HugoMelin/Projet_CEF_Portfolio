4e1239e978ed47dc9d71dc4913b9df5e1de4e942
HugoMelin <hugo.melin01@gmail.com>
2024-04-11 14:34:48 +0200

    Addfooter component

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { computed } from 'vue'
+
+    const copyright = computed(() =>{
+        const currentYear = new Date().getFullYear()
+        return `Copyright ${currentYear} | Hugo Melin`
+    });
+</script>
+
+<template>
+    <footer class="footer">
+        <p class="copyright">{{ copyright }}</p>
+    </footer>
+</template>
+
+<style scoped>
+    footer * {
+        background-color: #060640;
+        color: white;
+        padding: 10px;
+    }
+
+    .copyright {
+        text-align: center;
+        font-style: italic;
+    }
+</style>
