441b6ad804e68f390cba363013c5619005423baa
HugoMelin <hugo.melin01@gmail.com>
2024-04-12 12:16:53 +0200

    Add error message if inputs are empty

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,18 +1,48 @@
 <script setup>
     import TitreNiveau2 from '@/components/TitreNiveau2.vue'
+    import { ref, reactive } from 'vue'
+
+    const AfficheMessageErreur = ref(false)
+
+    const message = ref({
+        name: '',
+        mail: '',
+        tel: '',
+        object: '',
+        msg: '',
+    })
+
+    function sendMail(message) {
+        if (message.name.length < 1 
+        || message.mail.length < 1
+        || message.object.length < 1 
+        || message.msg.length < 1 ){
+            AfficheMessageErreur.value = true
+        }else {
+            AfficheMessageErreur.value = false
+            console.log(message.name.length)
+        }
+
+        console.log(AfficheMessageErreur.value)
+    }
 </script>
 
 <template>
     <div id="contact">
         <TitreNiveau2 class="contact__title">Contact</TitreNiveau2>
+
+        <div class="messageErreur" v-if="AfficheMessageErreur">
+            <span class="material-symbols-outlined">cancel</span>
+            <p>Tous les champs doivent être renseigné</p>
+        </div>
     
         <div class="form__grid">
-            <input type="text" name="name"  id="name" placeholder="Nom...">
-            <input type="email" name="mail" id="mail" placeholder="Email...">
-            <input type="tel" name="tel" id="tel" placeholder="Téléphone...">
-            <input type="text" name="object" id="object" placeholder="Objet...">
-            <textarea name="message" id="message" placeholder="Message..."></textarea>
-            <button class="CTA">Envoyer</button>
+            <input v-model="message.name" type="text" name="name"  id="name" placeholder="Nom...">
+            <input v-model="message.mail" type="email" name="mail" id="mail" placeholder="Email...">
+            <input v-model="message.tel" type="tel" name="tel" id="tel" placeholder="Téléphone...">
+            <input v-model="message.object" type="text" name="object" id="object" placeholder="Objet...">
+            <textarea v-model="message.msg" name="message" id="message" placeholder="Message..."></textarea>
+            <button class="CTA" @click="sendMail(message)">Envoyer</button>
         </div>
     </div>
 </template>
@@ -62,5 +92,19 @@
 
     #message {
         grid-row: 2 / 4;
+    }
+
+    .messageErreur {
+        display: flex;
+        align-items: center;
+        background-color: rgba(255, 0, 0, 0.35);
+        border: 1px solid red;
+        border-radius: 15px;
+        padding: 5px;
+    }
+
+    .messageErreur * {
+        color: red;
+        font-size: 0.9em;
     }
 </style>
