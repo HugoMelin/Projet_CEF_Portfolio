90b1f446cc0506d5d197e523231931c34f8cda3e
HugoMelin <hugo.melin01@gmail.com>
2024-04-17 09:27:33 +0200

    Add Error404 content

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,15 +1,39 @@
 <script setup>
-
+    import Project_SmallCard from '@/components/Project_SmallCard.vue';
+    import TitreNiveau2 from '@/components/TitreNiveau2.vue';
 </script>
 
 <template>
-    <h1>ERROR-404</h1>
-
-    <RouterLink to="/">Test</RouterLink>
+    <section>
+        <RouterLink class="error404" to="/">
+            <img src="@/assets/images/infinity.jpg" alt="">
+            <TitreNiveau2 class="title">ERROR-404</TitreNiveau2>
+            <p class="CTA">Revenir sur le site</p>
+        </RouterLink>
+    </section>
 </template>
 
 <style scoped>
-    h1 {
+    .error404 {
+        width: 60%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+        gap: 2em;
+        padding: 15px;
+        margin: auto;
+        text-decoration: none;
+    }
+
+    img {
+        width: 50%;
+        height: auto;
+        border-radius: 35%;
+        box-shadow: -4px 4px 25px #060640;
+    }
+
+    .title {
         font-size: 2em;
     }
 </style>
