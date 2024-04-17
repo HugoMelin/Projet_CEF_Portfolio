5c54603267a62aab3aef960a41ab6db2282575cc
HugoMelin <hugo.melin01@gmail.com>
2024-04-17 10:58:24 +0200

    Add meta discription and title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script setup>
   import Header from './components/Header.vue';
   import Footer from './components/Footer.vue';
-  import ContactForm from './components/ContactForm.vue';
-  import Presentation from './components/Presentation.vue';
-  import Projects from './components/Projects.vue';
 </script>
 
 <template>
